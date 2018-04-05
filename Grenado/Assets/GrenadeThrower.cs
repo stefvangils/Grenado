dc2b315c723a3f26929c028706c2a39ad9d919ea
stefvangils <saa.vangils@student.fontys.nl>
2018-04-05 20:05:53 +0200

    initial commit

---
+++ Grenado/Assets/GrenadeThrower.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeThrower : MonoBehaviour {
+
+    
+    public float throwForce = 40f;
+    public GameObject grenadePrefab;
+	
+
+	void Update () {
+        if (Input.GetMouseButtonDown(1))
+        {
+            ThrowGrenade();
+        }      
+    }
+
+    void ThrowGrenade()
+    {
+        GameObject grenade = Instantiate(grenadePrefab, transform.position + transform.forward * 1, transform.rotation);
+        Rigidbody rb = grenade.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * throwForce, ForceMode.VelocityChange);
+    }
+}
---
+++ Grenado/Assets/grenade.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grenade : MonoBehaviour {
+
+    public float delay = 3f;
+    public float radius = 5f;
+    public float force = 700f;
+
+    public GameObject explosionEffect;
+
+    float countdown;
+    bool hasExploded = false;
+	// Use this for initialization
+	void Start () {
+        countdown = delay;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        countdown -= Time.deltaTime;
+        if(countdown <= 0f && !hasExploded)
+        {
+            explode();
+            hasExploded = true;
+            Debug.Log(hasExploded);
+        }
+	}
+
+    void explode()
+    {
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        foreach(Collider nearbyObject in colliders)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if(rb != null)
+            {
+                Debug.Log(rb);
+                rb.AddExplosionForce(force, transform.position, radius);
+            }
+        }
+
+        Destroy(gameObject);
+    }
+}
